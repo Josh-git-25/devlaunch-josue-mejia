964c9cad0b494605ed560f0ffabaefbea4f44792
Josue Mejia <j-cr@hotmail.com>
2025-04-20 18:54:04 -0600

    chore(variables): add gym-membership exercise

---
+++ fundamentals/variables/gym-membership.js
@@ -0,0 +1,20 @@
+/*El Elite Fitness Center es un gimnasio boutique ubicado en Palo Alto, California, que ofrece membresia por $150 al mes.
+Opera de 6 AM a 10 PM, emplea a 10 entrenadores certificados y cuenta con comodidades como sauna, piscina y clases grupales.*/
+
+const gymBoutique = 'eliteFitnessCenter' 
+const location = 'Palo Alto CA'
+const membership = '150 dollars'
+
+const schedule = '6am to 10 pm'
+
+let trainers = 10 
+const amenities = 'sauna, pool, group classes'
+
+console.log (`
+    Gym name: ${gymBoutique}
+    Place: ${location}
+    cost per month: ${membership}
+    hours of operation: ${schedule}
+    Staff: ${trainers}
+    activities: ${amenities}
+    `)
